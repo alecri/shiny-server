59fb2baabafc8e31da1a26a7ac5c7d7c89fb318d
Alessio Crippa <alessio.crippa@gmail.com>
2016-09-02 14:44:00 +0200

    server and ui

---
+++ shinyForm/server.R
@@ -0,0 +1,106 @@
+# Alessio Crippa
+# September 2016
+
+# This is the server portion of a shiny app that "mimics" a Google form, in the
+# sense that it lets users enter some predefined fields and saves the answer
+# as a .csv file. Every submission is saved in its own file, so the results
+# must be concatenated together at the end
+
+library(shiny)
+library(shinyjs)
+library(digest)
+library(dplyr)
+library(DT)
+
+source("helpers.R")
+
+# create the directory
+formName <- "year-semester-basic-info"
+responsesDir <- file.path("data", formName)
+dir.create(responsesDir, recursive = TRUE, showWarnings = FALSE)
+
+# names of the fields on the form we want to save
+fieldsAll <- c("name", "favourite_pkg", "used_shiny", "r_num_years", "os_type")
+# mandatory fields
+fieldsMandatory <- c("name", "favourite_pkg")
+
+
+shinyServer(function(input, output, session) {
+   
+   observe({
+      # check if all mandatory fields have a value
+      mandatoryFilled <-
+         vapply(fieldsMandatory,
+                function(x) {
+                   !is.null(input[[x]]) && input[[x]] != ""
+                },
+                logical(1))
+      mandatoryFilled <- all(mandatoryFilled)
+      
+      # enable/disable the submit button
+      shinyjs::toggleState(id = "submit", condition = mandatoryFilled)
+   })
+   
+   formData <- reactive({
+      data <- sapply(fieldsAll, function(x) input[[x]])
+      data <- c(data, timestamp = epochTime())
+      data <- t(data)
+      data
+   })
+   
+   saveData <- function(data) {
+      fileName <- sprintf("%s_%s.csv",
+                          humanTime(),
+                          digest::digest(data))
+      
+      write.csv(x = data, file = file.path(responsesDir, fileName),
+                row.names = FALSE, quote = TRUE)
+   }
+   
+   # action to take when submit button is pressed
+   observeEvent(input$submit, {
+      saveData(formData())
+   })
+   
+   # action to take when submit button is pressed
+   observeEvent(input$submit, {
+      shinyjs::disable("submit")
+      shinyjs::show("submit_msg")
+      shinyjs::hide("error")
+      
+      tryCatch({
+         saveData(formData())
+         shinyjs::reset("form")
+         shinyjs::hide("form")
+         shinyjs::show("thankyou_msg")
+      },
+      error = function(err) {
+         shinyjs::html("error_msg", err$message)
+         shinyjs::show(id = "error", anim = TRUE, animType = "fade")
+      },
+      finally = {
+         shinyjs::enable("submit")
+         shinyjs::hide("submit_msg")
+      })
+   })
+   
+   observeEvent(input$submit_another, {
+      shinyjs::show("form")
+      shinyjs::hide("thankyou_msg")
+   }) 
+   
+   output$responsesTable <- DT::renderDataTable(
+      loadData(),
+      rownames = FALSE,
+      options = list(searching = FALSE, lengthChange = FALSE)
+   )
+   
+   output$downloadBtn <- downloadHandler(
+      filename = function() { 
+         sprintf("mimic-google-form_%s.csv", humanTime())
+      },
+      content = function(file) {
+         write.csv(loadData(), file, row.names = FALSE)
+      }
+   )   
+})
---
+++ shinyForm/ui.R
@@ -0,0 +1,47 @@
+# Alessio Crippa
+# September 2016
+
+# This is the ui portion of a shiny app that "mimics" a Google form, in the
+# sense that it lets users enter some predefined fields and saves the answer
+# as a .csv file. Every submission is saved in its own file, so the results
+# must be concatenated together at the end
+
+library(shiny)
+library(shinyjs)
+
+shinyUI(fluidPage(
+
+   shinyjs::useShinyjs(),
+   shinyjs::inlineCSS(appCSS),
+   
+   title = "Course Basic Student Info",
+   h2("Course Basic Student Info"),
+   
+   div(
+   id = "form",
+   textInput("name", "Name *", ""),
+   textInput("favourite_pkg", "Favourite R package"),
+   checkboxInput("used_shiny", "I've built a Shiny app in R before", FALSE),
+   sliderInput("r_num_years", "Number of years using R", 0, 25, 2, ticks = FALSE),
+   selectInput("os_type", "Operating system used most frequently",
+               c("",  "Windows", "Mac", "Linux")),
+   actionButton("submit", "Submit", class = "btn-primary"),
+   downloadButton("downloadBtn", "Download responses"),
+   DT::dataTableOutput("responsesTable"),
+   shiny::hr(),
+   shinyjs::hidden(
+      span(id = "submit_msg", "Submitting..."),
+      div(id = "error",
+          div(br(), tags$b("Error: "), span(id = "error_msg"))
+      )
+   )),
+   shiny::hr(),
+   
+   shinyjs::hidden(
+      div(
+         id = "thankyou_msg",
+         h3("Thanks, your response was submitted successfully!"),
+         actionLink("submit_another", "Submit another response")
+      )
+   )
+))
