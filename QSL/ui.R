23ef26986e15110c8a4a92e75c3b727d13c46c41
Alessio Crippa <alessio.crippa@gmail.com>
2016-10-21 10:31:32 +0200

    QSL update

--- QSL/ui.R
+++ QSL/ui.R
@@ -2,38 +2,49 @@ library(shiny)
 library(dygraphs)
 library(timevis)
 
-shinyUI(navbarPage("Quit Smoking Line",
-   #headerPanel("Quit Smoking Line"),
-   #h2("Evaluating the Impact of Health Warnings", align = "center"),
-   tabPanel(
-     "Homepage",
-     fluidPage(
-       h1("Evaluating the Impact of Health Warnings"),
-       h4("Designed by ", a("A. Crippa", target="_blank", target = "_blank", 
-                            href = "http://alecri.github.io/"))),
-     h3("Policy Events and News Media in the Tobacco Sector 1998-2016"),
-     timevisOutput("timeline")
-   ),
+shinyUI(fluidPage(
+   "Quit Smoking Line",
+   h1("Evaluating the Impact of Health Warnings"),
+   h4("Designed by ", a("A. Crippa", target="_blank", target = "_blank", 
+                        href = "http://alecri.github.io/")),
+   h3("Policy Events and News Media in the Tobacco Sector 1998-2016"),
    
-   tabPanel("Time Serie",
    sidebarLayout(
       sidebarPanel(
-         numericInput("months", label = "Months to Predict", 
-                      value = 72, min = 12, max = 144, step = 12),
-         selectInput("interval", label = "Prediction Interval",
-                     choices = c("0.80", "0.90", "0.95", "0.99"),
-                     selected = "0.95"),
-         checkboxInput("showgrid", label = "Show Grid", value = TRUE),
-         hr(),
-         div(strong("From: "), textOutput("from", inline = TRUE)),
-         div(strong("To: "), textOutput("to", inline = TRUE)),
-         br(),
+         checkboxInput("mod_lin", label = "Linear trend", value = FALSE),
+         checkboxInput("mod_season", label = "Seasonal trend", value = FALSE),
          helpText("Click and drag to zoom in (double click to zoom back out).")
       ),
       
       mainPanel(
-         dygraphOutput("dygraph")
+         tabsetPanel(
+            type = "tabs", 
+            tabPanel(
+               "Homepage",
+               timevisOutput("timeline")
+            ),
+            tabPanel("Interrupted Time Serie",
+                     dygraphOutput("its_graph"),
+                     conditionalPanel(
+                        "input.mod_lin == true",
+                        uiOutput("effect_lin")
+                     ),
+                     conditionalPanel(
+                        "input.mod_season == true",
+                        uiOutput("effect_season")
+                     )
+            ),
+            tabPanel("Analytical results",
+                     conditionalPanel(
+                        "input.mod_lin == true",
+                        verbatimTextOutput("modlin")
+                     ),
+                     conditionalPanel(
+                        "input.mod_season == true",
+                        verbatimTextOutput("modseason")
+                     )
+            )
+         )
       )
-   )
    )
 ))
