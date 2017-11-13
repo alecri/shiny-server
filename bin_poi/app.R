1b2ecf55e455dbf8c08ee45736a483c663c157ae
Alessio Crippa <alessio.crippa@gmail.com>
2017-11-13 14:44:26 +0100

    update

--- bin_poi/app.R
+++ bin_poi/app.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(shiny)
+library(DT)
+library(htmlTable)
+library(htmlwidgets)
 
 ui <- pageWithSidebar(
   headerPanel('Comparison between Binomial and Poisson distribution'),
@@ -9,25 +12,38 @@ ui <- pageWithSidebar(
     sliderInput("p", label = "Probability of success (n)", min = 0, 
                 max = 1, value = .1),
     sliderInput("lambda", label = "Expected number of success (lambda)", 
-                min = 0, 
-                max = 100, value = 50)
+                min = 0, max = 100, value = 50),
+    sliderInput("x", label = "Choose value", 
+                min = 0, max = 100, value = 50)
   ),
   mainPanel(
-    plotOutput('dist_plot')
+    plotOutput('dist_plot'),
+    br(),
+    checkboxInput("show", "Show probability on graph", value = FALSE),
+    tableOutput('prob')
   )
 )
-server <- function(input, output, session) {
-  
-  observeEvent(input$n,
-               updateSliderInput(session, "lambda",
-                                 max = floor(input$n*input$p + 3*input$n*input$p),
-                                 value = input$n*input$p)
-  )
-  observeEvent(input$p,
-               updateSliderInput(session, "lambda",
-                                 max = floor(input$n*input$p + 3*input$n*input$p),
-                                 value = input$n*input$p)
-  )
+server <- function(input, output, session){
+  observeEvent(input$n, {
+    updateSliderInput(session, "lambda",
+                      min = max(0,floor(input$n*input$p - 3*sqrt(input$n*input$p))),
+                      max = floor(input$n*input$p + 3*sqrt(input$n*input$p)),
+                      value = input$n*input$p)
+    updateSliderInput(session, "x",
+                      min = max(0,floor(input$n*input$p - 2*sqrt(input$n*input$p))),
+                      max = floor(input$n*input$p +  2*sqrt(input$n*input$p)),
+                      value = input$n*input$p)
+  })
+  observeEvent(input$p, {
+    updateSliderInput(session, "lambda",
+                      min = max(0,floor(input$n*input$p - 3*sqrt(input$n*input$p))),
+                      max = floor(input$n*input$p + 3*sqrt(input$n*input$p)),
+                      value = input$n*input$p)
+    updateSliderInput(session, "x",
+                      min = max(0,floor(input$n*input$p - 2*sqrt(input$n*input$p))),
+                      max = floor(input$n*input$p + 2*sqrt(input$n*input$p)),
+                      value = input$n*input$p)
+  })
   observeEvent(input$lambda,
                updateSliderInput(session, "p",
                                  value = input$lambda/input$n)
@@ -40,17 +56,31 @@ server <- function(input, output, session) {
       x = seq(floor(lambda - 3*sqrt(lambda)), floor(lambda + 3*sqrt(lambda))),
       d_bin = dbinom(x, size = input$n, prob = input$p),
       d_poi = dpois(x, lambda = lambda)
-    )
+    ) %>% gather(dist, p, -x)
     data_dist
   })
   
   output$dist_plot <- renderPlot({
-    ggplot(dat(), aes(x, d_bin)) +
-      geom_line(aes(col = "Binomial")) +
-      geom_line(aes(y = d_poi, col = "Poisson")) +
-      ylab("density") +
+    dist_plot <- ggplot(dat(), aes(x, p, col = dist)) +
+      geom_line() +
+      labs(y = "density", col = "distribution") + 
       theme_classic()
+    if (input$show)
+      dist_plot <- dist_plot + geom_area(data = subset(dat(), x <= input$x), aes(fill = dist), 
+                                         position = "dodge", alpha = .5) +
+        guides(col = FALSE)
+    dist_plot
   })
+  
+  output$prob <- renderTable({
+    prob <- matrix(c(dbinom(input$x, input$n, input$p), dpois(input$x, input$lambda),
+                    pbinom(input$x, input$n, input$p), ppois(input$x, input$lambda),
+                    pbinom(input$x, input$n, input$p, lower.tail = F), 
+                    ppois(input$x, input$lambda, lower.tail = F)),
+                  ncol = 2, nrow = 3, byrow = T)
+    dimnames(prob) <- list(c("P(X = x)", "P(X < x)", "P(X > x)"), c("Binomial", "Poisson"))
+    prob
+  }, rownames = T)
   
 }
 
