d459a95d3ae2f6fe17337d73cf8e5a6abcb04e14
Alessio Crippa <alessio.crippa@gmail.com>
2019-12-02 16:31:24 +0100

    change in the app

--- PC_inc_mort/app.R
+++ PC_inc_mort/app.R
@@ -5,7 +5,11 @@
 #' Designed by Alessio Crippa 191129
 
 # libraries needed
-pacman::p_load(shiny, tidyverse, plotly, ggthemes, DT)
+library(shiny)
+library(tidyverse)
+library(plotly)
+library(ggthemes)
+library(DT)
 theme_set(theme_bw())
 
 # load data from incidence-mortality-PC.R
@@ -21,9 +25,9 @@ ui <- navbarPage(
                      selectInput("rate", "Rate:", 
                                  choices = list("Incidence & mortality" = "all", "Incidence" = "incidence",
                                                 "Mortality" = "mortality"), selected = "all"),
-                     selectInput("region", "Region:", unique(indices$Region), selected = "Riket", multiple = T),
+                     selectInput("region", "Region:", unique(indices$Region), selected = "Sweden", multiple = T),
                      selectInput("age_cat", "Category of age:", 
-                                 choices = unique(indices$age_cat), selected = "Totalt", multiple = T),
+                                 choices = unique(indices$`Age category`), selected = "Totalt", multiple = T),
                      sliderInput("year_range", "Years range", sep = "",
                                  min = 1970, max = 2018, value = c(2000, 2018)),
                      width = 3
@@ -40,9 +44,10 @@ ui <- navbarPage(
              sidebarLayout(
                  sidebarPanel(
                      selectInput("age_cat_map", "Category of age:", 
-                                 choices = unique(indices$age_cat), selected = "Totalt", multiple = F),
+                                 choices = unique(indices$`Age category`), selected = "Totalt", multiple = F),
                      sliderInput("year", "Years range", sep = "",
-                                 min = 1970, max = 2018, value = 2000, animate = T),
+                                 min = 1970, max = 2018, value = 2000, 
+                                 animate = animationOptions(interval = 3000, loop = TRUE)),
                      p("See the gif", tags$a(href="https://alecri.github.io/downloads/map_rate.gif", "here")),
                      width = 3
                  ),
@@ -66,40 +71,40 @@ server <- function(input, output) {
     dat <- reactive({
         dat <- filter(indices, 
                       Region %in% input$region, 
-                      age_cat %in% input$age_cat,
-                      År <= input$year_range[2] &  År >= input$year_range[1]
+                      `Age category` %in% input$age_cat,
+                      Year <= input$year_range[2] &  Year >= input$year_range[1]
         )
-        if (input$rate != "all") dat <- filter(dat, rate == input$rate)
+        if (input$rate != "all") dat <- filter(dat, Rate == input$rate)
         dat
     })
     
     output$p_rate <- renderPlotly({
-        p <- ggplot(dat(), aes(År, value, linetype = rate, col = Region)) +
+        p <- ggplot(dat(), aes(Year, value, linetype = Rate, col = Region)) +
             geom_point() +
             geom_line() +
-            labs(x = "Year", y = "Rate (x 100 000)") +
-            facet_wrap(age_cat ~ ., scale = "free")
+            labs(y = "Rate (x 100 000)") +
+            facet_wrap(`Age category` ~ ., scale = "free")
         ggplotly(p)
     })
     
     output$tab_rate <- renderDataTable({
         dat() %>% 
-            pivot_wider(names_from = "rate", values_from = value)
+            pivot_wider(names_from = "Rate", values_from = value)
     })
     
     
     # Second panel: reactive data and output 
     dat_map <- reactive({
         dat_map <- filter(map_indices,
-                          age_cat == input$age_cat_map,
-                          År  == input$year
+                          `Age category` == input$age_cat_map,
+                          Year  == input$year
         )
         dat_map
     })
     
     output$p_map_inc <- renderPlotly({
         ggplotly(
-            filter(dat_map(), rate == "incidence") %>%
+            filter(dat_map(), Rate == "Incidence") %>%
                 ggplot(aes(ggplot_long, ggplot_lat, group = Region, fill = value)) +
                 geom_polygon() +
                 coord_equal() +
@@ -111,7 +116,7 @@ server <- function(input, output) {
     })
     output$p_map_mort <- renderPlotly({
         ggplotly(
-            filter(dat_map(), rate == "mortality") %>%
+            filter(dat_map(), Rate == "Mortality") %>%
                 ggplot(aes(ggplot_long, ggplot_lat, group = Region, fill = value)) +
                 geom_polygon() +
                 coord_equal() +
@@ -124,9 +129,9 @@ server <- function(input, output) {
     
     output$tab_rate_map <- renderDataTable({
         dat_map() %>% 
-            select(Region, År, age_cat, rate, value) %>% 
+            select(Region, Year, `Age category`, Rate, value) %>% 
             distinct() %>% 
-            pivot_wider(id_cols = c("Region", "År", "age_cat"), names_from = "rate", 
+            pivot_wider(id_cols = c("Region", "Year", "Age category"), names_from = "Rate", 
                         values_from = value)
     })
     
